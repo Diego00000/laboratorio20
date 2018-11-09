65a506174c7547ce1eb2f9c0c1d80a9cf7012bd7
diego00000 <diegohere24@gmail.com>
2018-11-09 13:47:52 -0500

    ejercicios

---
+++ js/lab.js
@@ -0,0 +1,38 @@
+//** ejercicio 1 */
+
+/*var valorPresente =prompt("ingrese su capital actual",valorPresente);
+var ValorPresente;
+var mat= valorPresente*Math.pow(1+0.02,60);
+
+console.log("valor consignado es: ",valorPresente);
+console.log("valor futuro en 5 años",mat);*/
+
+
+
+
+//** ejercicio 2 */
+
+/*var quantity
+var kil=4200
+
+
+var quantity=prompt("ingrese los kilos a comprar",quantity);
+console.log("los kilos a comprar son los siguientes: ",quantity);
+if (quantity<=2  ) {
+   
+    des=quantity*kil*0/100;
+    
+}
+if (quantity>2.1 && quantity <=5.0) {
+    des= quantity*kil*10/100;
+    
+}
+if (quantity>=5.1 && quantity <=10) {
+    des= quantity*kil*15/100;
+}
+if (quantity >=10.1) {
+    des= quantity*kil*20/100;
+}
+console.log("la compra de kilos tiene un valor de: ",quantity*kil);
+console.log("el valor con descuento a pagar es: ",quantity*kil-des);*/
+
